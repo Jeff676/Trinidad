45242f8597bcac515e7fc835c8c741158c1bf561
Jeff676 <luisfernandocolmenarez@gmail.com>
2025-05-17 18:27:00 -0400

    Panel administrativo + vista medicos + vista pacientes solo frontend

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -4,7 +4,7 @@ import { useUserStore } from '../stores/user';
 
 const userStore = useUserStore();
 
-const items = ref([  
+const items = ref([
     {
         label: 'Inicio',
         route: '/'
@@ -35,28 +35,29 @@ const items = ref([
 </script>
 
 <template>
-    <Menubar v-if="!userStore.isLoggedIn" :model="items" class="justify-content-end align-items-center nav-ppl border-round-top-lg my-0">
-      <template #item="{ item }">
-        <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom class="text-white no-underline" style="margin-left: 20px; font-size: 24px;">
-            <a v-ripple :href="href" @click="navigate">
-                <span>{{ item.label }}</span>
-            </a>
-        </router-link>
-      </template>
+    <Menubar v-if="!userStore.isLoggedIn" :model="items"
+        class="justify-content-end align-items-center nav-ppl border-round-top-lg my-0">
+        <template #item="{ item }">
+            <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom
+                class="text-white no-underline" style="margin-left: 20px; font-size: 24px;">
+                <a v-ripple :href="href" @click="navigate">
+                    <span>{{ item.label }}</span>
+                </a>
+            </router-link>
+        </template>
     </Menubar>
 
-    <nav v-if="userStore.isLoggedIn" class="col-12 flex justify-content-end align-items-center nav-ppl border-round-top-lg my-0">
-      <div  class="flex gap-3 align-items-center m-0">
-        <RouterLink to="/panel" class="text-white no-underline" style="margin-left: 20px; font-size: 24px;">Panel</RouterLink> 
-        <RouterLink to="/doctors" class="text-white no-underline" style="margin-left: 20px; font-size: 24px;">Doctores</RouterLink>
-        <RouterLink to="to" class="text-white no-underline"style="margin-left: 20px; font-size: 24px;">
-            <span @click="userStore.logout">Salir</span>
-        </RouterLink>
-      </div>
+    <nav v-if="userStore.isLoggedIn"
+        class="col-12 flex justify-content-end align-items-center nav-ppl border-round-top-lg my-0">
+        <div class="flex gap-3 align-items-center m-0">
+            <RouterLink to="/panel" class="text-white no-underline" style="margin-left: 20px; font-size: 24px;">Panel
+            </RouterLink>
+            <RouterLink to="/doctors" class="text-white no-underline" style="margin-left: 20px; font-size: 24px;">
+                Doctores
+            </RouterLink>
+            <RouterLink to="#" class="text-white no-underline" style="margin-left: 20px; font-size: 24px;">
+                <span @click="userStore.logout">Salir</span>
+            </RouterLink>
+        </div>
     </nav>
 </template>
-
-
-
-
-     
